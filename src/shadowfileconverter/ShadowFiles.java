41f3031e70aecbb17f9d0a5e3c2b9e625b147c9b
вапрол <@192.168.1.4>
2015-01-25 22:23:43 +0400

    Created the main class with file tools

---
+++ src/shadowfileconverter/ShadowFiles.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shadowfileconverter;
+
+/**
+ *
+ * @author Ruslan Feshchenko
+ * @version
+ */
+public class ShadowFiles {
+
+    /**
+     * Main constructor
+     * @param write false - open for reading, true - open for writing
+     */
+    public ShadowFiles(boolean write) {
+        if (write) {
+            
+        }
+    }
+
+}
