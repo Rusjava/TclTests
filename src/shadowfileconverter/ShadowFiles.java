277f4590902bf223e0ad51b91934607794c072e5
Samsung <Samsung@127.0.0.1>
2015-01-27 20:16:34 +0400

    Expended ShadowFiles class

--- src/shadowfileconverter/ShadowFiles.java
+++ src/shadowfileconverter/ShadowFiles.java
@@ -23,32 +23,21 @@ public class ShadowFiles {
     
     private boolean write;
     private boolean binary;
-    private File file;
-    private Object stream;
+    private File file=null;
+    private Object stream=null;
 
     /**
      * Main constructor
-     * @param write false - open for reading, true - open for writing
-     * @param binary false - open for text I/O, true - open for binary I/O
+     * @param wr false - open for reading, true - open for writing
+     * @param bi false - open for text I/O, true - open for binary I/O
+     * @throws java.io.FileNotFoundException
      */
     public ShadowFiles(boolean wr, boolean bi) throws FileNotFoundException {
         this.write=wr;
         this.binary=bi;
         if (write) {
             if (binary) {
-                JFileChooser fo=new JFileChooser ();
-                fo.setDialogTitle("Choose a binary file to save a ray set in");
-                int ans=fo.showOpenDialog(null);   
-                if (ans==JFileChooser.APPROVE_OPTION) {
-                    file=fo.getSelectedFile();
-                    if (file.exists ()) {
-                        int n=JOptionPane.showConfirmDialog(null, "The file already exists. Overwrite?", "Warning",
-                                JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-                        if (n==JOptionPane.NO_OPTION) {
-                            file=null;
-                            return;
-                        }
-                    }
+                if (openWrite("Choose a binary file to save a ray set in")) {
                     stream=new DataOutputStream(new FileOutputStream(file, false));
                 }      
             } else {
@@ -85,14 +74,45 @@ public class ShadowFiles {
     }
     
     /**
-     * Writes data for one ray
-     * @param raydata - double array of 18 numbers representing 18 columns of ray data
+     * Writes binary data for one ray or the file heading
+     * @param rayData double array of 18 numbers representing 18 columns of ray data
+     * @throws java.io.IOException
      */
-    public void write(double [] raydata) throws IOException {
+    public void write(double [] rayData) throws IOException {
         if (binary) {
-            for (int i=0; i<raydata.length; i++) {
-                ((DataOutputStream)stream).writeDouble(raydata[i]);
+            ((DataOutputStream)stream).write(new byte [] {18,0,0,0});
+            for (int i=0; i<rayData.length; i++) {
+                ((DataOutputStream)stream).writeDouble(rayData[i]);
             }
+            ((DataOutputStream)stream).write(new byte [] {18,0,0,0});
+            ((DataOutputStream)stream).writeInt(0);
         }
+    }
+    
+    /**
+     * Reads binary data of one ray or of the file heading
+     * @param rayData double array of 18 numbers representing 18 columns of ray data
+     */
+    public void read(double [] rayData) {
+        
+    }
+    
+    private boolean openWrite(String title) {
+        JFileChooser fo=new JFileChooser ();
+        fo.setDialogTitle(title);
+        int ans=fo.showOpenDialog(null);   
+        if (ans==JFileChooser.APPROVE_OPTION) {
+            file=fo.getSelectedFile();
+            if (file.exists()) {
+                int n=JOptionPane.showConfirmDialog(null, "The file already exists. Overwrite?", "Warning",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+                if (n==JOptionPane.NO_OPTION) {
+                    file=null;
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
