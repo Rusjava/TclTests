8f10db74b5ce09b2bcd6e9aae07259af084e7687
Samsung <Samsung@172.20.10.2>
2016-08-23 00:43:10 +0400

    Creating test project

---
+++ src/tcltests/TclTests.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcltests;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import tclinterpreter.*;
+
+/**
+ *
+ * @author Samsung
+ */
+public class TclTests {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TclParser parser = new TclParser(new TCLLexer("set name1 n45;"));
+        try {
+            System.out.println(parser.parse());
+        } catch (TclParser.TclParserError ex) {
+            Logger.getLogger(TclTests.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
