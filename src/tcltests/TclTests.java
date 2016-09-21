f4a490795e6d8acb73f236620154459507c57d23
вапрол <@192.168.1.4>
2016-09-21 20:59:39 +0400

    Merge origin/master
    Conflicts:
    	.gitignore
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/tcltests/TclTests.java
+++ src/tcltests/TclTests.java
@@ -16,6 +16,8 @@
  */
 package tcltests;
 
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import tclinterpreter.*;
@@ -32,32 +34,39 @@ public class TclTests {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         /*TclInterpreter inter = new TclInterpreter(new TclParser(
-         new TclLexer("set name1 \\\n n45;\n\t set name2 \"gh \\\n\t n450\"; puts $name2; puts $name1$name2[set name3 $name1]; ")));
+         new TclLexer("set name1 \\\n n45;\n\t set name2 \"gh \\\n\t n450\"; puts $name2; puts $name1$name2[set name3 $name1]; ")), null, true);
          try {
          System.out.println(inter.run());
          } catch (TclParser.TclParserError ex) {
          Logger.getLogger(TclTests.class.getName()).log(Level.SEVERE, null, ex);
          }*/
+        /*TclParser parser = new TclParser(
+         new TclLexer("puts имя; puts [set nn 1;]; set имя1 1.0; set vr 2; unset vr; set vr 9.6e+6; set vrr 3.2e-6; set vrr \"3.2e-4\";"
+         + "puts [expr {($vr / ($имя1 * 2)+ 3.5e+10* ($vrr)*(2*0.5) - 4.6e+6/($имя1*2-1)*($имя1*3-2))};];"));
+         try {
+         System.out.println(parser.parse());
+         } catch (TclParser.TclParserError ex) {
+         Logger.getLogger(TclTests.class.getName()).log(Level.SEVERE, null, ex);
+         }*/
         
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
         AbstractTclInterpreter inter = new TclInterpreter(new TclParser(
-                new TclLexer("set name1 1.0; "
-                        + "puts [expr {(9.6e+6 / ($name1 * 2)+ 3.5e+10* (3.2e-4)*(2*0.5) - 4.6e+6/(1.0*2-1)*(1.0*3-2))};];")),
-        null, true);
+                new TclLexer("puts имя; puts [set nn 1.0;]; set имя1 1.0; set vr 2; unset vr; set vr 9.6e+6; set vrr 3.2e-6; set vrr \"3.2e-4\";"
+                        + "puts [expr {($vr / ($имя1 * 2)+ 3.5e+10* ($vrr)*(2*0.5) - 4.6e+6/($имя1*2-1)*($имя1*3-2))};];")),
+                null, true, stream, "cp1251");
         try {
-            System.out.println(inter.run());
+            inter.run();
         } catch (TclParser.TclParserError ex) {
             Logger.getLogger(TclTests.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        /*AbstractTclInterpreter inter = new TclInterpreter(new TclParser(
-                new TclLexer("set name1 1.0; expr {(9.6e+6 / ($name1 * 2)+ 3.5e+10* (((3.2e-4)))*(2*0.5) - 4.6e+6/(1.0*2-1)*(1.0*3-2))};")));
+        System.out.println(inter.getOutput());
         try {
-            System.out.println(inter.run());
-        } catch (TclParser.TclParserError ex) {
+            System.out.println(stream.toString("cp1251"));
+        } catch (UnsupportedEncodingException ex) {
             Logger.getLogger(TclTests.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
     }
 
 }
