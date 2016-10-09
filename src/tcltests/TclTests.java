8b9442002f6e67687cfacce8a006642222cf73fd
вапрол <@192.168.1.2>
2016-10-09 21:07:39 +0400

    More tests

--- src/tcltests/TclTests.java
+++ src/tcltests/TclTests.java
@@ -51,11 +51,12 @@ public class TclTests {
          }*/
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         AbstractTclInterpreter inter = new TclInterpreter(new TclParser(
-                new TclLexer("puts {yes}; puts [set nn [expr {(2.0**10)+3*1};];]; set имя1 0.0; set vr 2; unset vr; set vr(0) 9.6e+6; set vr(1) 3.2e-6; set vr(1) \"3.2e-4\";"
+                new TclLexer("puts {23}; puts [set nn [expr {(2.0**10)+3*1};];]; set имя1 0.0; set vr 2; unset vr; set vr(0) 9.6e+6; set vr(1) 3.2e-6; set vr(1) \"3.2e-4\";"
                         + "puts [expr {($vr(0) / (-!$имя1 * -2)+ 3.5e+10* ($vr(1))*(~~2*0.5) - 4.6e+6/(!$имя1*2-1)*(!$имя1*3-2))};];"
                         + "puts [expr {((1<<012)+3)%10};]; puts [expr {\"string\"+\"1\" ne \"string2\"};];"
                         + "puts [set vr(1);]; puts [set vr(0);];"
-                        + "puts [if [expr {$vr(0)<1};] then 35 elseif [expr {$vr(0)<1};] then 35 else 45];")),
+                        + "puts [if [expr {$vr(0)<1};] then 25 elseif [expr {$vr(0)>1};] 35 else 45];"
+                        + "set vv 22; puts [set vv];")),
                 null, true, stream, "cp1251");
         try {
             inter.run();
